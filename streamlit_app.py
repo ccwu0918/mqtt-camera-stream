a7cd8d9b1331932f3c5f0ec418850396baee2193
Chia-Chun Wu <ccwu0918@gmail.com>
2022-05-23 17:11:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,55 @@
+import os
+import time
+
+import numpy as np
 import streamlit as st
-import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from helpers import byte_array_to_pil_image, get_config, get_now_string
+from mqtt import get_mqtt_client
+from paho.mqtt import client as mqtt
+from PIL import Image
 
-st.write("""
-# Simple Iris Flower Prediction App
+CONFIG_FILE_PATH = os.getenv("MQTT_CAMERA_CONFIG", "./config/config.yml")
+CONFIG = get_config(CONFIG_FILE_PATH)
 
-This app predicts the **Iris flower** type!
-""")
+MQTT_BROKER = CONFIG["mqtt"]["broker"]
+MQTT_PORT = CONFIG["mqtt"]["port"]
+MQTT_QOS = CONFIG["mqtt"]["QOS"]
 
-st.sidebar.header('User Input Parameters')
+MQTT_TOPIC = CONFIG["save-captures"]["mqtt_topic"]
 
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
+VIEWER_WIDTH = 600
 
-df = user_input_features()
 
-st.subheader('User Input parameters')
-st.write(df)
+def get_random_numpy():
+    """Return a dummy frame."""
+    return np.random.randint(0, 100, size=(32, 32))
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
 
-clf = RandomForestClassifier()
-clf.fit(X, Y)
+title = st.title(MQTT_TOPIC)
+viewer = st.image(get_random_numpy(), width=VIEWER_WIDTH)
 
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    st.write(f"Connected with result code {str(rc)} to MQTT broker on {MQTT_BROKER}")
 
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
 
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    if msg.topic != MQTT_TOPIC:
+        return
+    image = byte_array_to_pil_image(msg.payload)
+    image = image.convert("RGB")
+    viewer.image(image, width=VIEWER_WIDTH)
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+
+def main():
+    client = get_mqtt_client()
+    client.on_message = on_message
+    client.connect(MQTT_BROKER, port=MQTT_PORT)
+    client.subscribe(MQTT_TOPIC)
+    time.sleep(4)  # Wait for connection setup to complete
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
